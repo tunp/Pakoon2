7f707360141a553a38b518ac61cccb26317bf535
jettis <jje-git@jv.jetro.fi>
2023-03-28 22:05:59 +0300

    on emscripten go back in browser history when exiting game

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -57,7 +57,12 @@ void mainLoop(CPakoon1View *pakoon1) {
 }
 
 void emscripten_loop(void *pakoon1) {
-  mainLoop((CPakoon1View *)pakoon1);
+  if (((CPakoon1View *)pakoon1)->isExit()) {
+    emscripten_run_script("history.back()");
+    emscripten_cancel_main_loop();
+  } else {
+    mainLoop((CPakoon1View *)pakoon1);
+  }
 }
 
 int main(int argc, char **argv) {
